48fb96a97504ddb5e1ed813e827855656f070327
UberStash <expiredserver@gmail.com>
2021-06-27 10:50:00 -0400

    first commit

---
+++ src/homepage.component.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+
+const Homepage = () => (
+    <div className='hompage'>
+        <div className='directory-menu'>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>Presses</h1>
+                    <span className='subtitle'>Shop Now!</span>
+
+                </div>
+            </div>
+
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>Paper</h1>
+                    <span className='subtitle'>Shop Now!</span>
+
+                </div>
+            </div>
+
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>Bags</h1>
+                    <span className='subtitle'>Shop Now!</span>
+
+                </div>
+            </div>
+
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>Containers</h1>
+                    <span className='subtitle'>Shop Now!</span>
+
+                </div>
+            </div>
+
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>Accessories</h1>
+                    <span className='subtitle'>Shop Now!</span>
+
+                </div>
+            </div>
+
+        </div>
+    </div>
+);
+
+export default Homepage;
